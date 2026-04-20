7baa7acaa3f27f533222897fd7d4c6390c3805ad
a25jiayeylin <a25jiayeylin@inspedralbes.cat>
2026-04-20 14:04:52 +0200

    añadir el codigo de algunos php

---
+++ DIG+SOS/detall_incidencia.php
@@ -0,0 +1,14 @@
+<?php include_once " "; ?>
+<div class="row">
+    <div class="col-12">
+        <h1>Detall de l'incidencia</h1>
+        <form action=" " method="POST">
+            <div class="form-group">
+                <label for="descripcion">Descripción</label>
+                <textarea placeholder="Descripción curta de l'incidencia" class="form-control" name="descripcion" id="descripcion" cols="30" rows="10" required></textarea>
+            </div>
+            <div class="form-group"><button class="btn btn-success">Guardar</button></div>
+        </form>
+    </div>
+</div>
+<?php include_once " "; ?>
