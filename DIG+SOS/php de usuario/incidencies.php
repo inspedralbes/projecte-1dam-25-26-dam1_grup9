94d179fb4da3b826ea3df04a0d0d86379435fc54
Marc Lance De Los Reyes Fajardo <a25marfajdel@inspedralbes.cat>
2026-04-28 09:06:28 +0200

    Corregir variables

--- DIG+SOS/php de usuario/incidencies.php
+++ DIG+SOS/php de usuario/incidencies.php
@@ -1,14 +1,8 @@
 <?php
 $mysqli = include_once "connexio.php";
-$nombre = $_POST["nom_incidencia"];
+$nom = $_POST["nom_incidencia"];
 $departament_id = $_POST["departament_id"];
-$descripcion = $_POST["descripcio"];
+$descripcio = $_POST["descripcio"];
 $sentencia = $mysqli->prepare("INSERT INTO Incidencies (nom_incidencia, departament_id, descripcio) VALUES (?, ?, ?)");
-$sentencia->bind_param("sis", $nom_incidencia, $departament_id, $descripcio);
+$sentencia->bind_param("sis", $nom, $departament_id, $descripcio);
 $sentencia->execute();
-
-
-
-
-
-
