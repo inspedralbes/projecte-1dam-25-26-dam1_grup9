af76f217cb4e447771b01da7fe3b81fea255ac11
Marc Lance De Los Reyes Fajardo <a25marfajdel@inspedralbes.cat>
2026-04-28 09:35:56 +0200

    Correcció

--- DIG+SOS/php de usuario/connexio.php
+++ DIG+SOS/php de usuario/connexio.php
@@ -3,9 +3,7 @@ $host = "localhost";
 $usuari = "admin";
 $contrasenia = "ProjecteFinal_12345.";
 $base_de_dades = "db";
-$dbname = "Incidencies";
-
-$mysqli = new mysqli($host, $base_de_dades, $usuari, $contrasenia, $dbname);
+$mysqli = new mysqli($host, $base_de_dades, $usuari, $contrasenia);
 if ($mysqli->connect_errno) {
     echo "Ha fallat la connexió a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
--- DIG+SOS/php de usuario/incidencies.php
+++ DIG+SOS/php de usuario/incidencies.php
@@ -6,3 +6,4 @@ $descripcio = $_POST["descripcio"];
 $sentencia = $mysqli->prepare("INSERT INTO Incidencies (nom_incidencia, departament_id, descripcio) VALUES (?, ?, ?)");
 $sentencia->bind_param("sis", $nom, $departament_id, $descripcio);
 $sentencia->execute();
+?>
