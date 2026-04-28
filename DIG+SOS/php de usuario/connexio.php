889c1397cf53e5306733c33015af9c69537c2546
Marc Lance De Los Reyes Fajardo <a25marfajdel@inspedralbes.cat>
2026-04-28 08:54:46 +0200

    Correcció de fitxers PHP

--- DIG+SOS/php de usuario/connexio.php
+++ DIG+SOS/php de usuario/connexio.php
@@ -1,12 +1,12 @@
 <?php
-$host = "";
+$host = "localhost";
 $usuari = "admin";
 $contrasenia = "ProjecteFinal_12345.";
 $base_de_dades = "db";
 $dbname = "Incidencies";
 
-$mysqli = new mysqli($base_de_dades, $usuari, $contrasenia, $dbname);
+$mysqli = new mysqli($host, $base_de_dades, $usuari, $contrasenia, $dbname);
 if ($mysqli->connect_errno) {
-    echo "Falló la conexión a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
+    echo "Ha fallat la connexió a MySQL: (" . $mysqli->connect_errno . ") " . $mysqli->connect_error;
 }
 return $mysqli;
