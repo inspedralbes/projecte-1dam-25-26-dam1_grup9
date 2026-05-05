8160fcf129659fe0b57e02e59585d064139d1ac2
a25jiayeylin-tech <a25jiayeylin@inspedralbes.cat>
2026-05-05 10:55:00 +0200

    mejoras

---
+++ php/lista_prioritat.php
@@ -0,0 +1,102 @@
+<?php  
+require_once "connexion.php";
+
+$result = $conn->query("SELECT * FROM incidencies");
+$incidencies = $result->fetch_all(MYSQLI_ASSOC);
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Incidències no resoltes</title>
+
+    <style>
+      
+
+        table {
+            
+            border-collapse: collapse;
+            width: 100%;
+            
+        }
+
+        th, td {
+            border: 1px solid black;
+            padding: 10px;
+        }
+
+        th {
+            background-color: #2b5de7;
+            color: white;
+        }
+
+        .botones {
+            padding: 6px 12px;
+            background: #2c51f1;
+            text-decoration: none;
+            color: white;
+            border-radius: 5px;
+        }
+
+        .botones:hover {
+            background: #54a7df;
+        }
+
+        .inicio {
+            text-decoration: none;
+            padding: 10px 20px;
+            background-color: #2d59e9;
+            border: none;
+            color: white;
+        }
+
+        .inicio:hover {
+            background-color: #55a5da;
+        }
+    </style>
+</head>
+
+<body>
+
+<div class="box">
+
+    <h2>Incidències NO resoltes</h2>
+
+    <table>
+        <tr>
+            <th>ID</th>
+            <th>Departament</th>
+            <th>Data</th>
+            <th>Prioritat</th>
+            <th>Acció</th>
+        </tr>
+
+        <?php if (count($incidencies) > 0): ?>
+            <?php foreach ($incidencies as $i): ?>
+                <tr>
+                    <td><?= $i['id'] ?></td>
+                    <td><?= htmlspecialchars($i['departament']) ?></td>
+                    <td><?= $i['data_obertura'] ?></td>
+                    <td><?= $i['prioritat'] ? $i['prioritat'] : '-' ?></td>
+                    <td>
+                        <a class="botones" href="editar_actuacio.php?id=<?= $i['id'] ?>">
+                            Editar
+                        </a>
+                    </td>
+                </tr>
+            <?php endforeach; ?>
+        <?php else: ?>
+            <tr>
+                <td colspan="5">No hi ha incidències pendents</td>
+            </tr>
+        <?php endif; ?>
+
+        
+    </table>
+    <br>
+    <a href="responsable_tecnico.php" class="inicio">Cancelar</a>
+</div>
+
+</body>
+</html>
---
+++ php/responsable_tecnico.php
@@ -0,0 +1,59 @@
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Opciones</title>
+    <style>
+        body {
+            text-align: center;
+            height: 100vh;
+            font-family: Arial;
+            background: linear-gradient(135deg, #0648c4be, #25117e);
+            color: white;
+            padding: 15px 35px;
+            font-size: 18px;
+            font-weight: 600;
+            border-radius: 10px;
+            
+        }
+    
+    	button {
+            display: block;
+            width: 200px;
+            margin: 10px auto;
+            padding: 12px;
+            font-size: 16px;
+            cursor: pointer;
+        }
+        
+        .inicio {
+            text-decoration: none;
+            padding: 10px 20px;
+            background-color: #2d59e9;
+            border: none;
+            color: white;
+        }
+
+        .inicio:hover {
+            background-color: #55a5da;
+        }
+    </style>
+</head>
+<body >
+
+<div>
+
+    
+    <form action="lista_prioritat.php" method="get">
+        <button type="submit">Modificar incidencia</button>
+    </form>
+
+
+    <br><br>
+    <a href="index.php" class="inicio">Inicio</a>
+
+</div>
+
+</body>
+</html>
