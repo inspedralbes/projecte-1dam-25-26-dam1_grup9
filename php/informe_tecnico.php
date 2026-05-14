cf5c262e5fbd547c3bb41f5bfbb030999f34e991
a25jiayeylin-tech <a25jiayeylin@inspedralbes.cat>
2026-05-14 21:29:44 +0200

    comentarios

--- php/informe_tecnico.php
+++ php/informe_tecnico.php
@@ -4,15 +4,18 @@ session_start();
 require_once "connexion.php";
 include_once "logger.php";
 
-//El código de opciones te envía el ID por URL (GET), 
-// pero esta línea comprueba de forma obligatoria si el ID está guardado en la SESIÓN ($_SESSION).
+// Esta línea comprueba si el ID está guardado en la SESIÓN ($_SESSION).
 if (!isset($_SESSION['tecnic_id'])) {
-     // Si no se encuentra en la sesión, te echa y te manda al selector de técnicos
+     // Si no se encuentra en la sesión, te echa y te manda a que eligas uno de los técnicos
     header("Location: elegir_tecnico.php");
     exit();
 }
+//Cogemos el id que nos envio
 $id_seleccionat = $_SESSION['tecnic_id'];
 
+// Este sql lo que hace es coger el nombre de tecnico, la id de la incidencia, data de creación, el rango de prioridad, el tiempo y si no 
+//hay que ponga 0. Donde cada incidencia esta asignada a un tecnico. Si hay actuaciones que se muetre y si no hay tambien.
+// Debe solo coger incidencia que no estan solucionadas y deben estar agrupados por la id incidencia (y entre otros). Ademas de estar ordenado pro prioridad 
 $sql = ("SELECT t.nom AS tecnic, i.id AS incidencia, i.data_obertura, i.prioritat, COALESCE(SUM(a.temps), 0) AS temps_total
     FROM tecnics t
     JOIN incidencies i ON i.tecnic_id = t.id
@@ -26,18 +29,22 @@ $sql = ("SELECT t.nom AS tecnic, i.id AS incidencia, i.data_obertura, i.priorita
                 WHEN 'Baixa' THEN 3
             END
 ");
-
+//prepara la consulta sql
 $stmt = $conn->prepare($sql);
+// Aseguramos que la variable ? que se envie sea un numero 
 $stmt->bind_param("i", $id_seleccionat);
 $stmt->execute();
-
+//Obtener el resultado
 $res = $stmt->get_result();
+//Conviertes el resultado en un array (fetch_all) y MYSQLI_ASSOC usa como etiquetas los nombres de columnas.
 $data = $res->fetch_all(MYSQLI_ASSOC);
 
 $result = [];
 
 foreach ($data as $row) {
+     //guardamos el nombre del tecnico
     $nom = $row['tecnic'];
+     //creamos grupos por tecnico donde dentro de cada tecnico tiene sus incidencias ordenadas por prioridad
     $result[$row['tecnic']][$row['prioritat']][] = $row;
 }
 ?>
@@ -121,6 +128,7 @@ foreach ($data as $row) {
                     <th>Prioritat</th>
                 </tr>
                 <?php
+                    //si no hay incidencia asignadas a este tecnico muestra un mensaje
                     $existe = false;
                     $ordre = ['Alta', 'Mitja', 'Baixa'];
 
@@ -143,7 +151,7 @@ foreach ($data as $row) {
 
                 if (!$existe): ?>
                     <tr>
-                        <td colspan="4">No hi ha incidències obertes</td>
+                        <td colspan="4">No hi ha incidències asignadas</td>
                     </tr>
                 <?php endif; ?>
 
