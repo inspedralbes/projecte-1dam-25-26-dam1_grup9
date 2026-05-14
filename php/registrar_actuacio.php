9d5598cb9c2e9451d171a0dd296544ceab879d5f
a25jiayeylin-tech <a25jiayeylin@inspedralbes.cat>
2026-05-14 23:36:13 +0200

    comentario

--- php/registrar_actuacio.php
+++ php/registrar_actuacio.php
@@ -2,32 +2,38 @@
 require_once 'connexion.php';
 include_once "logger.php";
 
+// Obtenemos el id que teniamos antes 
+// Si no existe se le asignará el valor null por defecto usando el operador ??
 $id = $_GET['id'] ?? null;
 
+// Si no hay un id se mostrara un mensaje de error
 if (!$id) {
     die("Error: falta ID de incidencia");
 }
 
-
+// Si se ha pulsado el botón de "guardar" haz esto
 if (isset($_POST['guardar'])) {
-
+    // Se recoge los datos que se escribió en el formulario
     $desc = $_POST['descripcio'];
     $temps = $_POST['temps'];
     $visible = isset($_POST['visible']) ? 1 : 0;
 
+    // Prepara la consulta para insertar los valores
+    // 'NOW()' es una función de MySQL que inserta automáticamente la fecha y hora actuales
     $stmt = $conn->prepare("INSERT INTO actuacions
         (incidencia_id, data_actuacio, descripcio, temps, visible)
         VALUES (?, NOW(), ?, ?, ?)
     ");
-
+     // Ejecuta la consulta pasando las variables en orden dentro de un array
     $stmt->execute([$id, $desc, $temps, $visible]);
 }
 
-
+// Si se ha pulsado el botón de "tancar" haz esto
 if (isset($_POST['tancar'])) {
-
+     // Recoge la fecha de cierre que se puso en el formulario
     $data_final = $_POST['data_final'];
 
+    // Prepara la consulta de la actualización para cambiar el estado de la incidencia
     $stmt = $conn->prepare("UPDATE incidencies
         SET resolta = 1,
             data_tancament = ?
@@ -37,13 +43,17 @@ if (isset($_POST['tancar'])) {
     $stmt->execute([$data_final, $id]);
 }
 
+// Prepara otro consulta 
 $stmtInc = $conn->prepare("SELECT data_tancament, data_obertura FROM incidencies WHERE id = ?");
 $stmtInc->execute([$id]);
+//Saca el resultado de la bbdd y lo transforma en una array.
 $incidencia = $stmtInc->get_result()->fetch_assoc();
+//Guarda las fechas
 $data_fi = $incidencia['data_tancament'] ?? null;
 $data_inici = $incidencia['data_obertura'] ?? '' ;
 
-
+// Prepara la consulta para extraer los datos y despues ordenarlo por
+// fecha de cada actuacion hechp desde la más antigua hasta la más reciente.
 $stmt = $conn->prepare(" SELECT data_actuacio, descripcio, temps, visible
     FROM actuacions
     WHERE incidencia_id = ?
@@ -212,18 +222,20 @@ $actuacions = $resultat->fetch_all(MYSQLI_ASSOC);
        
         <script>
             function validarForm(){
-
+                //coge la fecha de la creación de la incidencia y la fecha de finalizacio que se puso
                 let data_inici =new Date("<?= $data_inici ?>");
                 let data_fi = document.getElementById("data_final").value;
 
                 let error = "";
 
+                // Convierte el texto del calendario en una fecha real para poder comparar
                 let dataFi = new Date(data_fi);
 
+                //Si la fecha de fin es menor a la fecha de creación daria un mensaje de error
                 if(dataFi < data_inici){
                     error = "La data de finalització no pot ser menor a la data de creació de la incidència<br>";
                 }
-
+                //Qualquier error no se enviara a la bbdd
                 if(error != ""){
                     document.getElementById("error").innerHTML = error;
                     return false; 
